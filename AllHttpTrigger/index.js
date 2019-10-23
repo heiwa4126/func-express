e4be02f2bad52ab3599a967e843bf443f1ea722d
Masato Ota <ota-masato@nexs.nec.co.jp>
2019-10-23 16:38:28 +0900

    auto deply test

--- AllHttpTrigger/index.js
+++ AllHttpTrigger/index.js
@@ -14,7 +14,7 @@ app.get('/api/:foo/:bar', (req, res) => {
 app.get('/api/hello', (req, res) => {
   req.context.log({ hello: 'world' }) // log example
   res.set('Content-Type', 'text/plain; charset=utf-8')
-  res.send("Hello World! It's " + new Date().toISOString())
+  res.send("Hello World!!! It's " + new Date().toISOString())
 })
 
 // Binds the express app to an Azure Function handler
