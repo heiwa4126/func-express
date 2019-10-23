808329f749c850b7e765c954f64c16c288f37c77
Your Name <you@example.com>
2019-10-23 15:52:54 +0900

    Initial commit

---
+++ HttpTrigger1/index.js
@@ -0,0 +1,20 @@
+const createHandler = require('azure-function-express').createHandler
+const express = require('express')
+
+// Create express app as usual
+const app = express()
+
+app.get('/api/:foo/:bar', (req, res) => {
+  res.json({
+    foo: req.params.foo,
+    bar: req.params.bar
+  })
+})
+
+app.get('/api/hello', (req, res) => {
+  res.set('Content-Type', 'text/plain; charset=utf-8')
+  res.send("Hello World! It's " + new Date().toISOString())
+})
+
+// Binds the express app to an Azure Function handler
+module.exports = createHandler(app)
